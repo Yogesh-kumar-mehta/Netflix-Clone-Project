e9da63a2831e43694120dde3133aa874c0c28900
Yogesh Kumar <myk79887@gmail.com>
2024-05-12 14:34:55 +0530

    initial commit

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const API_KEY = "132e9f8d3e9fbf2e567f1f5646578f4a";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+
+//0079cc9ef0dad5b23695597aeacef53e1
+// 132e9f8d3e9fbf2e567f1f5646578f4a
